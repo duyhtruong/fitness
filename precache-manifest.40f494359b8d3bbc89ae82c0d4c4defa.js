d8f9ace5b5a0344dbf6ac925f914882c964227c1
Duy Truong <duytruongh@gmail.com>
2019-07-24 15:04:27 -0700

    Updates

---
+++ precache-manifest.40f494359b8d3bbc89ae82c0d4c4defa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eaf90fb26d904e93bb59add2394d3b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d429fbeedc935b607b",
+    "url": "/static/css/2.32a52a2a.chunk.css"
+  },
+  {
+    "revision": "312abfaf8e20b3a570ef",
+    "url": "/static/css/main.dc110f57.chunk.css"
+  },
+  {
+    "revision": "53d429fbeedc935b607b",
+    "url": "/static/js/2.9e00568b.chunk.js"
+  },
+  {
+    "revision": "312abfaf8e20b3a570ef",
+    "url": "/static/js/main.d0a1ac12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a10e2753be140799ef4e0ff1db96557",
+    "url": "/static/media/fitness_logo3.5a10e275.png"
+  }
+]);
